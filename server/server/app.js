cf9898f754a03cc37db3797e2fd538b67ea2ea56
Vadx <lv_km@ukr.net>
2021-03-03 22:56:52 +0200

    Add Part 1

--- server/server/app.js
+++ server/server/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+const uri = "mongodb+srv://test-user:Qcv3feXFFhrk5bvB@cluster0.bkwjw.mongodb.net/graphql_db?retryWrites=true&w=majority";
+
+// mongoose.connect('mongodb+srv://test-user:Qcv3feXFFhrk5bvB@cluster0.bkwjw.mongodb.net/graphql_db?retryWrites=true&w=majority');
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// test-user
+// Qcv3feXFFhrk5bvB
+//mongodb+srv://vadx:<password>@cluster0.bkwjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, err => {
   err ? console.log(err) : console.log('Server started!');
